8d32335a5f004ef9e46a6ad3756ad2177fd0b4fe
cford <kcford26@gmail.com>
2021-10-10 14:52:15 -0400

    agents human/random

--- agents.py
+++ agents.py
@@ -1,10 +1,20 @@
 from game import Agent
+from random import randrange
 
 class HumanAgent(Agent):
-    "a human player"
+    "a human player interface"
+    def getMove(self, board):
+        tile = input("tile to move (x,y):")
+        dir = input("direction to move (N/S/E/W):")
+        jumps = input("number of jumps:")
+        #check for validitiy
 
 class RandomAgent(Agent):
     "an agent that takes random valid moves"
+    def getMove(self, board):
+        legalmoves = board.getValidMoves(self)
+        rand = randrange(len(legalmoves))
+        return legalmoves[rand]
 
 class MiniMaxAgent(Agent):
     "an agent that searches for moves with minimax"
