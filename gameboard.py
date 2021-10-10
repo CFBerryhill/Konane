e1e5b6dd04a4dba0cc0c7409713de580ccf7345f
Paul <hebertp@lafayette.edu>
2021-10-10 14:57:31 -0400

    Started GamBoard

---
+++ gameboard.py
@@ -0,0 +1,28 @@
+
+
+
+class Move:
+
+    def __init__(self, tile, dir, jumps):
+        self.tile = tile
+        self.dir = dir
+        self.jumps = jumps
+
+
+class GameBoard:
+
+    def __init__(self):
+        ""
+
+    def fooBoard(self, board, move):
+
+        return board
+
+    def copyBoard(self, board):
+
+        copy = board
+
+        return copy
+
+    def validMoves(self, board, Plaer):
+        ""
